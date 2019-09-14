8b8427f588d0fe537e917c782c23add038d07aa9
matttodd <matttodd6@gmail.com>
2019-09-14 16:43:24 -0400

    routing works

--- app/views.py
+++ app/views.py
@@ -1,9 +1,9 @@
-from flask import Blueprint, render_template, abort
+from flask import Blueprint, render_template, abort, request
 from jinja2 import TemplateNotFound
 
 sample_page = Blueprint('sample_page', 'sample_page', template_folder='templates')
-test_page = Blueprint('test_page', 'test_page', template_folder='templates')
-
+home_page = Blueprint('home_page', 'home_page', template_folder='templates')
+results_page = Blueprint('results_page', 'results_page', template_folder='templates')
 
 @sample_page.route('/sample')
 def get_sample():
@@ -12,9 +12,18 @@ def get_sample():
     except TemplateNotFound:
         abort(404)
 
-@sample_page.route('/test')
+@sample_page.route('/home')
 def get_test():
     try:
-        return render_template('test.html')
+        return render_template('home.html')
+    except TemplateNotFound:
+        abort(404)
+
+@sample_page.route('/results', methods=['POST'])
+def get_results():
+    article = request.form['article']
+    
+    try:
+        return render_template('results.html', article=article)
     except TemplateNotFound:
         abort(404)
