3d975213ab5bcf8ae3dbf07e08a309de6635f775
Aryton Hoi <aryton.a.hoi@gmail.com>
2019-09-15 04:28:55 -0400

    added testing and title topic extraction

--- nlp/article.py
+++ nlp/article.py
@@ -1,4 +1,4 @@
-from counter_point.nlp.nlp import NLP
+from nlp.nlp import NLP
 import tldextract #library for url domain name extraction
 
 # represents an Article
@@ -10,24 +10,31 @@ class Article:
         self.text_content = text_content # article text
         
         # values computed later
-        self.news_source = None # (Ex. CNN or Fox)
-        self.topics = [] # list of topic objects
+        self.source_name = None # (Ex. CNN or Fox)
+        self.topics = [] # list of topic objects from content
+        self.title_topics = [] # list of topics from title
         self.citations = [] #list of citation objects
         self.date = None # date of article
 
         self.nlp = NLP()
+        self.get_source_name()
         self.get_topics()
+        
    
     # gets source name (Ex. CNN or Fox) of Article based on url
     def get_source_name(self):
         extraction = tldextract.extract(self.url)
-        self.news_source = extraction.domain
+        self.source_name = extraction.domain
 
     # Uses GCloud NPL api to parse text for topics (entities)
     # topics are keywords to the article
-    def get_topics(self, salience_threshold=0.01,language='en'):
-        self.topics = self.nlp.get_topics(self.text_content,salience_threshold,
-                    title=self.title,language=language)
+    def get_topics(self, salience_threshold=0.02,language='en'):
+        self.topics = self.nlp.get_topics(self.text_content,salience_threshold,language=language)
+        if self.title:
+            self.title_topics = self.nlp.get_topics(self.title,salience_threshold,language=language)
+        return None        
+
+    def get_keywords(self):
         return None
 
     # Gets other articles that agree with this one
@@ -45,23 +52,16 @@ class Article:
 
     # prints topic info
     def print_topic_info(self):
-        if self.topics: # check not empty
-            for topic in self.topics:
-                salience = topic.salience
-                sentiment = topic.sentiment
+        for topic_list in [self.title_topics,self.topics]:
+            if topic_list:
+                print('Topics Set ...')
+                for topic in topic_list:
+                    salience = topic.salience
+                    sentiment = topic.sentiment
 
-                statement = "Topic:{} type:{} salience:{:.2f} sentiment:{:.2f},{:.2f}"
-                print(statement.format(topic.topic_name,
-                                    topic.topic_type,
-                                    topic.salience,
-                                    topic.sentiment.score,
-                                    topic.sentiment.magnitude))
-
-
-# if __name__ == '__main__':
-#     title = 'A plan to end gun violence from students who survived it'
-#     content = 'They demanded change and ignited a grass-roots movement that has given youthful new vigor to the fight for gun safety. Now, these young activists have put forward a bold gun-control proposal that aims to reframe the debate on gun policy.'
-
-#     a = Article('rip.com',title,content)    
-#     print(a.text_content)
-#     a.print_topic_info()
+                    statement = "Topic:{} type:{} salience:{:.2f} sentiment:{:.2f},{:.2f}"
+                    print(statement.format(topic.topic_name,
+                                        topic.topic_type,
+                                        topic.salience,
+                                        topic.sentiment.score,
+                                        topic.sentiment.magnitude))
