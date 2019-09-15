213b0d97489fc514d29fb1f46c3b31c4f543b846
Aryton Hoi <aryton.a.hoi@gmail.com>
2019-09-15 08:10:46 -0400

    added opposing/agreeing story getting

--- test.py
+++ test.py
@@ -1,12 +1,15 @@
 from nlp.article import Article
 
 if __name__ == '__main__':
-  url = 'https://www.allsides.com/news/2019-08-23-0733/plan-end-gun-violence-students-who-survived-it'
-  title = 'Lara Trump: 2020 budget child care proposal will tackle very real crisis threatening American families'
+  url = 'https://www.foxnews.com/politics/california-newsom-abortion-pill-bill-public-university-campus'
+  title = 'California bill on \'abortion pill\' access at public universities goes to governor\'s desk'
   with open('fox1.txt', 'r') as file:
     content = file.read().replace('\n', '')
 
   a = Article(url,title,content)    
   print(a.source_name)
   # a.print_topic_info()
-  a.make_gsearch_query()
+  # a.make_gsearch_query()
+  a.get_agreeing_articles()
+  a.get_disagreeing_articles()
+  a.get_neutral_articles()
