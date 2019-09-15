9fa58221e23ed5e9c7d213dfee6338a47b1f9028
matttodd <matttodd6@gmail.com>
2019-09-14 22:54:53 -0400

    web = scraped

--- app/utils.py
+++ app/utils.py
@@ -2,6 +2,8 @@ from functools import wraps
 
 from flask import url_for, current_app, request, Response, Blueprint
 from flask_restplus import Api
+from bs4 import BeautifulSoup
+from bs4.element import Comment
 
 
 def add_basic_auth(blueprint: Blueprint, username, password, realm='api'):
@@ -63,3 +65,32 @@ class PatchedApi(Api):
         if current_app.config['TESTING'] or current_app.config['APP_ENVIRONMENT'] == 'development':
             print(e)
         super().handle_error(e)
+
+def tag_visible(element):
+    if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
+        return False
+    if isinstance(element, Comment):
+        return False
+    return True
+
+def text_from_html(body):
+    soup = BeautifulSoup(body, 'html.parser')
+    texts = soup.findAll(text=True)
+    visible_texts = list(filter(tag_visible, texts))
+    left_bound = 0
+    right_bound = len(visible_texts)
+    for i in range(len(visible_texts)):
+        if len(visible_texts[i]) > 35:
+            left_bound = i
+            break
+
+    for j in range(len(visible_texts)):
+        if len(visible_texts[len(visible_texts) - j - 1]) > 50:
+            right_bound = len(visible_texts) - j - 1
+            break
+    # print("##############")
+    # print(left_bound)
+    # print(right_bound)
+    visible_texts = visible_texts[left_bound:right_bound]
+    # print(list(visible_texts))
+    return u" ".join(t.strip() for t in visible_texts)
--- app/views.py
+++ app/views.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, render_template, abort, request
 from jinja2 import TemplateNotFound
+from bs4 import BeautifulSoup
+import requests
+from .utils import text_from_html
 
 sample_page = Blueprint('sample_page', 'sample_page', template_folder='templates')
 home_page = Blueprint('home_page', 'home_page', template_folder='templates')
@@ -22,8 +25,24 @@ def get_test():
 @sample_page.route('/results', methods=['POST'])
 def get_results():
     article = request.form['article']
+    page = requests.get(article)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    title = soup.find('h1').get_text()
+    
+    # article_text = soup.find_all(['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'])
+    article_text = text_from_html(requests.get(article).content)
+    # concat_text = ""
+    # for tag in article_text:
+    #     concat_text += tag.get_text()
+    print("###########")
+    print(title)
+    print(article_text)
+
+    positive_list = []
+    neutral_list = []
+    negative_list = []
     
     try:
-        return render_template('results.html', article=article)
+        return render_template('results.html', url=article, article=('%.40s' % article), positive_list=positive_list, neutral_list=neutral_list, negative_list=negative_list)
     except TemplateNotFound:
         abort(404)
